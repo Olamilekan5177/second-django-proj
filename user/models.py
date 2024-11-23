75613a24c4cafa23505120a9e268bea368a55d0f
adelalu <adelaluwahab@gmail.com>
2024-11-23 22:26:43 +0100

    Added dj-database-url tooo22

--- user/models.py
+++ user/models.py
@@ -5,3 +5,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
+
+def __str__(self):
+        return f'{self.user.username} Profile'
