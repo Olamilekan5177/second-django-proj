3505cbc5f90315aaade610e503caa51ee9c327e3
adelalu <adelaluwahab@gmail.com>
2024-11-23 12:45:27 +0100

    userlast

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'IbIXzsbcWpvulghFjhDHyBmULYUYooia',
+
+        'HOST': 'autorack.proxy.rlwy.net',
+
+        'PORT': '41094',
     }
 }
+
+
 
 
 # Password validation
